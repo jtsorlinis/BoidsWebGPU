9e36b7badebd4b8393b424e304b0879d2ffa801a
Jason Tsorlinis <jtsorlinis@gmail.com>
2023-04-16 20:55:34 +1000

    refactored compute shader into functions

--- src/boidComputeShader.ts
+++ src/boidComputeShader.ts
@@ -19,19 +19,13 @@ fn getGridID(pos: vec2<u32>) -> u32 {
   return (params.gridDimX * pos.y) + pos.x;
 }
 
-@compute @workgroup_size(256)
-fn main(@builtin(global_invocation_id) GlobalInvocationID : vec3<u32>) {
-  var index : u32 = GlobalInvocationID.x;
-
-  var boid = boidsIn[index];
-  
-  // merged behaviours
-  var center : vec2<f32> = vec2<f32>(0.0, 0.0);
-  var close : vec2<f32> = vec2<f32>(0.0, 0.0);
-  var avgVel : vec2<f32> = vec2<f32>(0.0, 0.0);
+fn mergedBehaviours(boid: ptr<function,Boid>) {
+  var center : vec2<f32> = vec2<f32>();
+  var close : vec2<f32> = vec2<f32>();
+  var avgVel : vec2<f32> = vec2<f32>();
   var neighbours: u32 = 0u;
 
-  var gridXY = getGridLocation(boid);
+  var gridXY = getGridLocation(*boid);
   var cell = getGridID(gridXY);
 
   // Loop around cell
@@ -41,10 +35,10 @@ fn main(@builtin(global_invocation_id) GlobalInvocationID : vec3<u32>) {
 
     for (var i = start; i < end; i += 1) {
       var other = boidsIn[i];
-      var distance = distance(boid.pos, other.pos);
+      var distance = distance((*boid).pos, other.pos);
       if (distance > 0.0 && distance < params.visualRange) {
         if(distance < params.minDistance) {
-          close += boid.pos - other.pos;
+          close += (*boid).pos - other.pos;
         }
         center += other.pos;
         avgVel += other.vel;
@@ -56,33 +50,46 @@ fn main(@builtin(global_invocation_id) GlobalInvocationID : vec3<u32>) {
   if (neighbours > 0u) {
     center /= f32(neighbours);
     avgVel /= f32(neighbours);
-    boid.vel += (center - boid.pos) * (params.cohesionFactor * params.dt);
-    boid.vel += (avgVel - boid.vel) * (params.alignmentFactor * params.dt);
+    (*boid).vel += (center - (*boid).pos) * (params.cohesionFactor * params.dt);
+    (*boid).vel += (avgVel - (*boid).vel) * (params.alignmentFactor * params.dt);
   }
 
-  boid.vel += close * (params.separationFactor * params.dt);
+  (*boid).vel += close * (params.separationFactor * params.dt);
+}
 
-  // limit speed
-  var speed = length(boid.vel);
+fn limitSpeed(boid: ptr<function, Boid>) {
+  var speed = length((*boid).vel);
   if (speed > params.maxSpeed) {
-    boid.vel = normalize(boid.vel) * params.maxSpeed;
+    (*boid).vel = normalize((*boid).vel) * params.maxSpeed;
   }
   if (speed < params.minSpeed) {
-    boid.vel = normalize(boid.vel) * params.minSpeed;
+    (*boid).vel = normalize((*boid).vel) * params.minSpeed;
   }
+}
 
-  // keep in bounds
-  if (boid.pos.x < -params.xBound) {
-    boid.vel.x += params.turnSpeed * params.dt;
-  } else if (boid.pos.x > params.xBound) {
-    boid.vel.x -= params.turnSpeed * params.dt;
+fn keepInBounds(boid: ptr<function, Boid>) {
+  if ((*boid).pos.x < -params.xBound) {
+    (*boid).vel.x += params.turnSpeed * params.dt;
+  } else if ((*boid).pos.x > params.xBound) {
+    (*boid).vel.x -= params.turnSpeed * params.dt;
   }
-  if (boid.pos.y > params.yBound) {
-    boid.vel.y -= params.turnSpeed * params.dt;
-  } else if (boid.pos.y < -params.yBound) {
-    boid.vel.y += params.turnSpeed * params.dt;
+  if ((*boid).pos.y > params.yBound) {
+    (*boid).vel.y -= params.turnSpeed * params.dt;
+  } else if ((*boid).pos.y < -params.yBound) {
+    (*boid).vel.y += params.turnSpeed * params.dt;
   }
+}
 
+@compute @workgroup_size(256)
+fn main(@builtin(global_invocation_id) GlobalInvocationID : vec3<u32>) {
+  var index : u32 = GlobalInvocationID.x;
+
+  var boid = boidsIn[index];
+  
+  mergedBehaviours(&boid);
+  limitSpeed(&boid);
+  keepInBounds(&boid);
+  
   boid.pos += boid.vel * params.dt;
 
   boids[index] = boid;
