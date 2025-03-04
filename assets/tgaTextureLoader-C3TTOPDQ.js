ea0175a6f1589526c41e352e0ebb0ae44d66117a
Jason Tsorlinis <jason.tsorlinis@jbhifi.com.au>
2025-03-04 17:56:15 +1100

    Updates

--- assets/tgaTextureLoader-C3TTOPDQ.js
+++
@@ -1,1 +0,0 @@
-import{g as r,h as n}from"./index-BaHdmlDO.js";class d{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{d as _TGATextureLoader};
